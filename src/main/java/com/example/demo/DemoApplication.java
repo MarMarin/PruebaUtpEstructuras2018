b67c1d55d9df39dba89532549c0ba18066b7996a
MarMarin <violeta.castellanos@utp.edu.co>
2018-08-08 19:15:36 -0500

    Modificando archivo principal

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class DemoApplication {
+public 
+class DemoApplication {
+
+	
 
 	public static void main(String[] args) {
+
+
+		System.out.println("Hola mundo");
+		
 		SpringApplication.run(DemoApplication.class, args);
+	
 	}
+
 }
